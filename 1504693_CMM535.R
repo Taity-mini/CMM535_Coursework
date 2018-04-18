b8038202dc3ae65807feec166defb7da9a9bb756
Taity-mini <1504693@rgu.ac.uk>
2018-04-18 11:40:28 +0100

    First section complete and started latex report *Added helper function for import *Finished up to preprocessing part *latex report template started

--- 1504693_CMM535.R
+++ 1504693_CMM535.R
@@ -3,37 +3,196 @@ library(randomForest)
 library(e1071)  
 library(caret)  
 library(ggplot2) 
+library(gridExtra)
+library(caret)
+library(rpart.plot)
+library(RColorBrewer)
+library(plyr)
+#helper function
+source('helper_functions.r')
+
+#Import datasets using helper function
+
+mushroom <- fetchAndCleanData()
+
+#Data exploration
 
 
-#Import Dataset
-#Feature names
-colNames <- c("class", "cap-shape", "cap-surface",
-                 "cap-color", "bruises?", 
-                 "odor",
-                 "grill-attachment",
-                 "grill-spacing",
-                 "grill-size",
-                 "grill-color",
-                 "stalk-shape",
-                 "stalk-root",
-                 "stalk-surface-above-ring",
-                 "stalk-surface-below-ring",
-                 "stalk-color-above-ring",
-                 "stalk-color-below-ring",
-                 "veil-type",
-                 "veil-color",
-                 "ring-number",
-                 "ring-type",
-                 "spore-print-color",
-                 "population",
-                 "habitat")
-#Import datasets
+#Number of rows in the dataset
+nrow(mushroom)
+#Number of columns in the dataset
+ncol(mushroom)
 
-mushroom <- read.table("data/agaricus-lepiota.data" ,header = FALSE, sep = ",",
-                         strip.white = TRUE, col.names = colNames,
-                         na.strings = "?")
+#Summary of the dataset
 summary(mushroom)
+
 str(mushroom)
 
+#Inspecting the dataset
 
-#Preprocess dataset
+
+#Class Distribution
+barplot(table(mushroom$Edible))
+
+
+#Comparisons of CapShape and CapSurface with Edible or Poisionous
+ggplot(mushroom,aes(x=CapShape, y=CapSurface, color=Edible)) + 
+                       geom_jitter(alpha=0.3) +
+                       scale_color_manual(breaks = c('Edible','Poisonous'), values=c('darkgreen','red'))
+                                                                                  
+
+
+
+# ggplot(mushroom,aes(x=StalkSurfaceAboveRing, y=CapSurface, color=Edible)) + 
+#   geom_jitter(alpha=0.3) +
+#   scale_color_manual(breaks = c('Edible','Poisonous'), values=c('darkgreen','red'))
+
+
+#Comparisons of CapShape and CapSurface with Edible or Poisionous
+ggplot(mushroom,aes(x=CapShape, y=CapSurface, color=Edible)) + 
+  geom_jitter(alpha=0.3) +
+  scale_color_manual(breaks = c('Edible','Poisonous'), values=c('darkgreen','red'))
+
+
+
+
+
+
+##Preprocess dataset
+
+#Remove missing values
+table(complete.cases (mushroom))
+
+
+#Modeling and Classification
+
+#Divide dataset into training and testing 
+mushroom$Edible <- as.factor(mushroom$Edible)
+
+mushroomCopy <- mushroom
+
+
+
+#Divide the datset into 70% training and 30% testing.
+inTrain <- createDataPartition(y=mushroom$Edible, p=.7, list=FALSE)
+
+#Assign indexes to split the Mushroom dataset into training and testing
+training <- mushroom[inTrain,]
+testing <- mushroom[-inTrain,]
+
+ncol(training)
+
+ncol(testing)
+
+#Random forest classification 
+library(randomForest)
+library(doParallel) 
+cl <- makeCluster(detectCores(), type='PSOCK')
+registerDoParallel(cl)
+
+
+#Set initial trees value
+ntrees <- 100
+rf = randomForest(Edible ~ .,  
+                  ntree = 100,
+                  data = training)
+plot(rf)  
+
+
+set.seed(0)
+df <- data.frame(ntrees =as.numeric(),
+                 Accuracy=as.numeric())
+system.time((for(i in 1:10){
+  
+RFModel = randomForest(Edible ~ .,  
+                  ntree = ntrees,
+                  data = training,
+                  mtry = 10,
+                  proximity = TRUE)
+
+preds <- levels(training)[RFModel$test$predicted]
+
+auc <- sum(preds == testing[,23]/nrow(testing))*100
+df <- rbind(df, data.frame(NTrees = ntrees, Accuracy = auc))
+
+ntrees <- ntrees + 100
+}))
+plot(rfModel)
+
+varImpPlot(RFModel,main = 'Variable Importance')
+
+
+
+  
+
+
+
+
+for(i in 1:10){
+  RFModel <- randomForest(
+          training[,-23],
+          testing[,-23],
+          
+          xtest = testing[,-23],
+          ytest = testing[,23],
+          ntree = ntrees,
+          mtry = 10,
+          proximity = TRUE
+  )
+  
+  preds <- levels(training[,23])[RFModel$test$predicted]
+  
+  auc <- sum(preds == testing[,23]/nrow(testing))*100
+  df <- rbind(df, data.frame(NTrees = ntrees, Accuracy = auc))
+  
+  ntrees <- ntrees + 100
+}
+
+print(RFModel)
+# turn parallel processing off and run sequentially again:
+registerDoSEQ()
+# comparing the running time for each loop
+system.time(x <- foreach(j=1:times ) %dopar% check(j))  #  2.56 seconds  (notice that the first run would be slower, because of R's lazy loading)
+system.time(for(j in 1:times ) x <- check(j))  #  4.82 seconds
+
+# stop workers
+stopWorkers(workers)
+
+
+#Clustering Function
+clustData <- function (df,ClassIndex,kmeansClasses = rep(0,unique(df[,ClassIndex]))) {
+  # use split function to split the dataset according to the class label
+  # a set of dataframes each representing a class label will be stored
+  # in dfs list()
+  dfs <- split (df, df[,ClassIndex])
+  # create empty list
+  clustList <- list()
+  n <- length(dfs)
+  for (i in 1:length(kmeansClasses)){
+    # Cluster according to all features excluding the label
+    if (kmeansClasses[i]>1 & kmeansClasses[i]< nrow(dfs[[i]])){
+      clustList[[i]] <- kmeans(dfs[[i]][,-ClassIndex],kmeansClasses[i])
+      #plotcluster(clustList[[i]], clustList[[i]]$cluster)
+      dfs[[i]]$cluster <- paste0((dfs[[i]][,ClassIndex]),
+                                 "_","c",clustList[[i]]$cluster)
+    }
+    else {
+      dfs[[i]]$cluster = paste0((dfs[[i]][,ClassIndex]),
+                                "_c0")
+    }
+  }
+  # put all list elements in a dataframe and return it
+  # note that ldply() require the library plyr
+  allClusteredElements <- ldply (dfs, data.frame)
+  # drop the first column 'id' resulting from ldply
+  allClusteredElements <- allClusteredElements[,-1]
+  allClusteredElements <- allClusteredElements[,-ClassIndex]
+  return(allClusteredElements)
+}
+
+
+
+
+
+
+
---
+++ helper_functions.r
@@ -0,0 +1,253 @@
+#Helper function is from : https://raw.githubusercontent.com/stoltzmaniac/Mushroom-Classification/master/helper_functions.R
+fetchAndCleanData = function(){
+  
+  # All of this code is from
+  # https://rstudio-pubs-static.s3.amazonaws.com/125760_358e4a6802c94fa29e2a9ab49f45df94.html
+  # I only switched it into a function to make it cleaner in my analysis
+  
+  #theUrl = "https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
+  mushrooms = read.table("data/agaricus-lepiota.data", header = FALSE, sep = ",")
+  
+  
+  
+  #create a data frame with only the required columns
+  #two data frames are created since there is a column break
+  #shrooms1 = mushrooms[,1:4]
+  #shrooms2 = mushrooms[,6]
+  #the two are combined into one data frame
+  #shrooms = cbind(shrooms1, shrooms2)
+  shrooms = mushrooms
+  
+  #column names are added
+  colnames(shrooms) = c("Edible",
+                        "CapShape",
+                        "CapSurface",
+                        "CapColor",
+                        "Bruises",                        
+                        "Odor",
+                        "GillAttachment",
+                        "GillSpacing",
+                        "GillSize",
+                        "GillColor",
+                        "StalkShape",
+                        "StalkRoot",
+                        "StalkSurfaceAboveRing",
+                        "StalkSurfaceBelowRing",
+                        "StalkColorAboveRing",
+                        "StalkColorBelowRing",
+                        "VeilType",
+                        "VeilColor",
+                        "RingNumber",
+                        "RingType",
+                        "SporePrintColor",
+                        "Population",
+                        "Habitat")
+  
+  
+  #Edible
+  shrooms$Edible = as.character(shrooms$Edible)
+  shrooms$Edible[shrooms$Edible == "e"] = "Edible"
+  shrooms$Edible[shrooms$Edible == 'p'] = "Poisonous"
+  shrooms$Edible = factor(shrooms$Edible)
+  
+  
+  # Edible
+  #levels(shrooms$Edible) = c(levels(shrooms$Edible), c("Poisonous","Edible"))
+  #shrooms$Edible[shrooms$Edible == "p"] = "Poisonous"
+  #shrooms$Edible[shrooms$Edible == "e"] = "Edible"
+  
+  #CapShape
+  levels(shrooms$`CapShape`) = c(levels(shrooms$`CapShape`), c("Bell","Conical","Convex","Flat","Knobbed","Sunken"))
+  shrooms$`CapShape`[shrooms$`CapShape` == "b"] = "Bell"
+  shrooms$`CapShape`[shrooms$`CapShape` == "c"] = "Conical"
+  shrooms$`CapShape`[shrooms$`CapShape` == "x"] = "Convex"
+  shrooms$`CapShape`[shrooms$`CapShape` == "f"] = "Flat"
+  shrooms$`CapShape`[shrooms$`CapShape` == "k"] = "Knobbed"
+  shrooms$`CapShape`[shrooms$`CapShape` == "s"] = "Sunken"
+  
+  #CapSurface
+  levels(shrooms$`CapSurface`) = c(levels(shrooms$`CapSurface`), c("Fibrous", "Grooves", "Scaly", "Smooth"))
+  shrooms$`CapSurface`[shrooms$`CapSurface` == "f"] = "Fibrous"
+  shrooms$`CapSurface`[shrooms$`CapSurface` == "g"] = "Grooves"
+  shrooms$`CapSurface`[shrooms$`CapSurface` == "y"] = "Scaly"
+  shrooms$`CapSurface`[shrooms$`CapSurface` == "s"] = "Smooth"
+  
+  #CapColor
+  levels(shrooms$`CapColor`) = c(levels(shrooms$`CapColor`), c("Brown", "Buff", "Cinnamon", "Gray", "Green", "Pink", "Purple", "Red", "White", "Yellow"))
+  shrooms$`CapColor`[shrooms$`CapColor` == "n"] = "Brown"
+  shrooms$`CapColor`[shrooms$`CapColor` == "b"] = "Buff"
+  shrooms$`CapColor`[shrooms$`CapColor` == "c"] = "Cinnamon"
+  shrooms$`CapColor`[shrooms$`CapColor` == "g"] = "Gray"
+  shrooms$`CapColor`[shrooms$`CapColor` == "r"] = "Green"
+  shrooms$`CapColor`[shrooms$`CapColor` == "p"] = "Pink"
+  shrooms$`CapColor`[shrooms$`CapColor` == "u"] = "Purple"
+  shrooms$`CapColor`[shrooms$`CapColor` == "e"] = "Red"
+  shrooms$`CapColor`[shrooms$`CapColor` == "w"] = "White"
+  shrooms$`CapColor`[shrooms$`CapColor` == "y"] = "Yellow"
+  
+  # Bruises
+  levels(shrooms$Bruises) = c(levels(shrooms$Bruises), c("True","False"))
+  shrooms$Bruises[shrooms$Bruises == "t"] = "True"
+  shrooms$Bruises[shrooms$Bruises == "f"] = "False"
+  
+  #Odor
+  levels(shrooms$Odor) = c(levels(shrooms$Odor), c("Almond", "Anise", "Creosote", "Fishy", "Foul", "Musty", "None", "Pungent", "Spicy"))
+  shrooms$Odor[shrooms$Odor == "a"] = "Almond"
+  shrooms$Odor[shrooms$Odor == "l"] = "Anise"
+  shrooms$Odor[shrooms$Odor == "c"] = "Creosote"
+  shrooms$Odor[shrooms$Odor == "y"] = "Fishy"
+  shrooms$Odor[shrooms$Odor == "f"] = "Foul"
+  shrooms$Odor[shrooms$Odor == "m"] = "Musty"
+  shrooms$Odor[shrooms$Odor == "n"] = "None"
+  shrooms$Odor[shrooms$Odor == "p"] = "Pungent"
+  shrooms$Odor[shrooms$Odor == "s"] = "Spicy"
+  
+  # GillAttachment
+  levels(shrooms$GillAttachment) = c(levels(shrooms$GillAttachment), c("Attached","Descending","Free","Notched"))
+  shrooms$GillAttachment[shrooms$GillAttachment == "a"] = "Attached"
+  shrooms$GillAttachment[shrooms$GillAttachment == "d"] = "Descending"
+  shrooms$GillAttachment[shrooms$GillAttachment == "f"] = "Free"
+  shrooms$GillAttachment[shrooms$GillAttachment == "n"] = "Notched"
+  
+  # GillSpacing
+  levels(shrooms$GillSpacing) = c(levels(shrooms$GillSpacing), c("Close","Crowded","Distant"))
+  shrooms$GillSpacing[shrooms$GillSpacing == "c"] = "Close"
+  shrooms$GillSpacing[shrooms$GillSpacing == "w"] = "Crowded"
+  shrooms$GillSpacing[shrooms$GillSpacing == "d"] = "Distant"
+  
+  # GillSize
+  levels(shrooms$GillSize) = c(levels(shrooms$GillSize), c("Broad","Narrow"))
+  shrooms$GillSize[shrooms$GillSize == "b"] = "Broad"
+  shrooms$GillSize[shrooms$GillSize == "n"] = "Narrow"
+  
+  # GillColor
+  levels(shrooms$GillColor) = c(levels(shrooms$GillColor), c("Black","Brown","Buff","Chocolate","Gray","Green","Orange","Pink","Purple","Red","White","Yellow"))
+  shrooms$GillColor[shrooms$GillColor == "k"] = "Black"
+  shrooms$GillColor[shrooms$GillColor == "n"] = "Brown"
+  shrooms$GillColor[shrooms$GillColor == "b"] = "Buff"
+  shrooms$GillColor[shrooms$GillColor == "h"] = "Chocolate"
+  shrooms$GillColor[shrooms$GillColor == "g"] = "Gray"
+  shrooms$GillColor[shrooms$GillColor == "r"] = "Green"
+  shrooms$GillColor[shrooms$GillColor == "o"] = "Orange"
+  shrooms$GillColor[shrooms$GillColor == "p"] = "Pink"
+  shrooms$GillColor[shrooms$GillColor == "u"] = "Purple"
+  shrooms$GillColor[shrooms$GillColor == "e"] = "Red"
+  shrooms$GillColor[shrooms$GillColor == "w"] = "White"
+  shrooms$GillColor[shrooms$GillColor == "y"] = "Yellow"
+  
+  # StalkShape
+  levels(shrooms$StalkShape) = c(levels(shrooms$StalkShape), c("Enlarging","Tapering"))
+  shrooms$StalkShape[shrooms$StalkShape == "e"] = "Enlarging"
+  shrooms$StalkShape[shrooms$StalkShape == "t"] = "Tapering"
+  
+  # StalkRoot
+  levels(shrooms$StalkRoot) = c(levels(shrooms$StalkRoot), c("Bulbous","Club","Cup","Equal","Rhizomorphs","Rooted","Missing"))
+  shrooms$StalkRoot[shrooms$StalkRoot == "b"] = "Bulbous"
+  shrooms$StalkRoot[shrooms$StalkRoot == "c"] = "Club"
+  shrooms$StalkRoot[shrooms$StalkRoot == "u"] = "Cup"
+  shrooms$StalkRoot[shrooms$StalkRoot == "e"] = "Equal"
+  shrooms$StalkRoot[shrooms$StalkRoot == "z"] = "Rhizomorphs"
+  shrooms$StalkRoot[shrooms$StalkRoot == "r"] = "Rooted"
+  shrooms$StalkRoot[shrooms$StalkRoot == "?"] = "Missing"
+  
+  # StalkSurfaceAboveRing
+  levels(shrooms$StalkSurfaceAboveRing) = c(levels(shrooms$StalkSurfaceAboveRing), c("Fibrous","Scaly","Silky","Smooth"))
+  shrooms$StalkSurfaceAboveRing[shrooms$StalkSurfaceAboveRing == "f"] = "Fibrous"
+  shrooms$StalkSurfaceAboveRing[shrooms$StalkSurfaceAboveRing == "y"] = "Scaly"
+  shrooms$StalkSurfaceAboveRing[shrooms$StalkSurfaceAboveRing == "k"] = "Silky"
+  shrooms$StalkSurfaceAboveRing[shrooms$StalkSurfaceAboveRing == "s"] = "Smooth"
+  
+  # StalkSurfaceBelowRing
+  levels(shrooms$StalkSurfaceBelowRing) = c(levels(shrooms$StalkSurfaceBelowRing), c("Fibrous","Scaly","Silky","Smooth"))
+  shrooms$StalkSurfaceBelowRing[shrooms$StalkSurfaceBelowRing == "f"] = "Fibrous"
+  shrooms$StalkSurfaceBelowRing[shrooms$StalkSurfaceBelowRing == "y"] = "Scaly"
+  shrooms$StalkSurfaceBelowRing[shrooms$StalkSurfaceBelowRing == "k"] = "Silky"
+  shrooms$StalkSurfaceBelowRing[shrooms$StalkSurfaceBelowRing == "s"] = "Smooth"
+  
+  # StalkColorAboveRing
+  levels(shrooms$StalkColorAboveRing) = c(levels(shrooms$StalkColorAboveRing), c("Brown","Buff","Cinnamon","Gray","Orange","Pink","Red","White","Yellow"))
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "n"] = "Brown"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "b"] = "Buff"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "c"] = "Cinnamon"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "g"] = "Gray"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "o"] = "Orange"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "p"] = "Pink"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "e"] = "Red"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "w"] = "White"
+  shrooms$StalkColorAboveRing[shrooms$StalkColorAboveRing == "y"] = "Yellow"
+  
+  
+  # StalkColorBelowRing
+  levels(shrooms$StalkColorBelowRing) = c(levels(shrooms$StalkColorBelowRing), c("Brown","Buff","Cinnamon","Gray","Orange","Pink","Red","White","Yellow"))
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "n"] = "Brown"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "b"] = "Buff"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "c"] = "Cinnamon"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "g"] = "Gray"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "o"] = "Orange"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "p"] = "Pink"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "e"] = "Red"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "w"] = "White"
+  shrooms$StalkColorBelowRing[shrooms$StalkColorBelowRing == "y"] = "Yellow"
+  
+  # VeilType
+  levels(shrooms$VeilType) = c(levels(shrooms$VeilType), c("Partial","Universal"))
+  shrooms$VeilType[shrooms$VeilType == "p"] = "Partial"
+  shrooms$VeilType[shrooms$VeilType == "u"] = "Universal"
+  
+  # VeilColor
+  levels(shrooms$VeilColor) = c(levels(shrooms$VeilColor), c("Brown","Orange","White","Yellow"))
+  shrooms$VeilColor[shrooms$VeilColor == "n"] = "Brown"
+  shrooms$VeilColor[shrooms$VeilColor == "o"] = "Orange"
+  shrooms$VeilColor[shrooms$VeilColor == "w"] = "White"
+  shrooms$VeilColor[shrooms$VeilColor == "y"] = "Yellow"
+  
+  # RingNumber
+  levels(shrooms$RingNumber) = c(levels(shrooms$RingNumber), c("None","One","Two"))
+  shrooms$RingNumber[shrooms$RingNumber == "n"] = "None"
+  shrooms$RingNumber[shrooms$RingNumber == "o"] = "One"
+  shrooms$RingNumber[shrooms$RingNumber == "t"] = "Two"
+  
+  # RingType
+  levels(shrooms$RingType) = c(levels(shrooms$RingType), c("Cobwebby","Evanescent","Flaring","Large","None","Pendant","Sheathing","Zone"))
+  shrooms$RingType[shrooms$RingType == "c"] = "Cobwebby"
+  shrooms$RingType[shrooms$RingType == "e"] = "Evanescent"
+  shrooms$RingType[shrooms$RingType == "f"] = "Flaring"
+  shrooms$RingType[shrooms$RingType == "l"] = "Large"
+  shrooms$RingType[shrooms$RingType == "n"] = "None"
+  shrooms$RingType[shrooms$RingType == "p"] = "Pendant"
+  shrooms$RingType[shrooms$RingType == "s"] = "Sheathing"
+  shrooms$RingType[shrooms$RingType == "z"] = "Zone"
+  
+  # SporePrintColor
+  levels(shrooms$SporePrintColor) = c(levels(shrooms$SporePrintColor), c("Black","Brown","Buff","Chocolate","Green","Orange","Purple","White","Yellow"))
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "k"] = "Black"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "n"] = "Brown"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "b"] = "Buff"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "h"] = "Chocolate"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "r"] = "Green"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "o"] = "Orange"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "u"] = "Purple"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "w"] = "White"
+  shrooms$SporePrintColor[shrooms$SporePrintColor == "y"] = "Yellow"
+  
+  # Population
+  levels(shrooms$Population) = c(levels(shrooms$Population), c("Abundnant","Clustered","Numerous","Scattered","Several","Solitary"))
+  shrooms$Population[shrooms$Population == "a"] = "Abundnant"
+  shrooms$Population[shrooms$Population == "c"] = "Clustered"
+  shrooms$Population[shrooms$Population == "n"] = "Numerous"
+  shrooms$Population[shrooms$Population == "s"] = "Scattered"
+  shrooms$Population[shrooms$Population == "v"] = "Several"
+  shrooms$Population[shrooms$Population == "y"] = "Solitary"
+  
+  # Habitat
+  levels(shrooms$Habitat) = c(levels(shrooms$Habitat), c("Grasses","Leaves","Meadows","Paths","Urban","Waste","Woods"))
+  shrooms$Habitat[shrooms$Habitat == "g"] = "Grasses"
+  shrooms$Habitat[shrooms$Habitat == "l"] = "Leaves"
+  shrooms$Habitat[shrooms$Habitat == "m"] = "Meadows"
+  shrooms$Habitat[shrooms$Habitat == "p"] = "Paths"
+  shrooms$Habitat[shrooms$Habitat == "u"] = "Urban"
+  shrooms$Habitat[shrooms$Habitat == "w"] = "Waste"
+  shrooms$Habitat[shrooms$Habitat == "d"] = "Woods"
+  
+  return(shrooms)
+}
